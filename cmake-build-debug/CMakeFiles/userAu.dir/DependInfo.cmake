a51b3244d4f02c937cf151a96a72472bd29956f7
Tamas Nemeth <ithomas.nemeth@gmail.com>
2019-08-14 17:51:57 +0200

    Chore - Delete  unnecessary subfolder

---
+++ cmake-build-debug/CMakeFiles/userAu.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/inputParameterInspection.cpp" "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/cmake-build-debug/CMakeFiles/userAu.dir/inputParameterInspection.cpp.o"
+  "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/login.cpp" "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/cmake-build-debug/CMakeFiles/userAu.dir/login.cpp.o"
+  "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/main.cpp" "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/cmake-build-debug/CMakeFiles/userAu.dir/main.cpp.o"
+  "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/registration.cpp" "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/cmake-build-debug/CMakeFiles/userAu.dir/registration.cpp.o"
+  "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/registrationIntoDatabase.cpp" "/Users/tamasnemeth/Documents/Coding Projects/User_Autentication_Cpp/cmake-build-debug/CMakeFiles/userAu.dir/registrationIntoDatabase.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
